5ef3f81f14d7b1228f8c5d64ae5a4209e937baa9
Hyesung Oh <haesungoh414@gmail.com>
2023-02-13 02:39:46 +0900

    feat: generate routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { HomePageComponent } from "./pages/home-page/home-page.component";
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
+import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  { path: "login", component: LoginPageComponent },
+  { path: "**", component: NotFoundPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
 import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MatSlideToggleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
