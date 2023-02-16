d20ca022b6ad880ede7f67cc967d96273b197883
Hyesung Oh <haesungoh414@gmail.com>
2023-02-17 01:22:20 +0900

    feat: setting for google map, markup default event detail page

--- src/app/event/event-detail-page/event-detail-page.component.ts
+++ src/app/event/event-detail-page/event-detail-page.component.ts
@@ -1,8 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { Observable, of, switchMap } from "rxjs";
+
+interface Event {
+  id: string;
+}
 
 @Component({
   selector: "app-event-detail-page",
   templateUrl: "./event-detail-page.component.html",
   styleUrls: ["./event-detail-page.component.scss"],
 })
-export class EventDetailPageComponent {}
+export class EventDetailPageComponent implements OnInit {
+  declare googleMapApiLoaded: Observable<boolean>;
+
+  declare event$: Observable<Event>;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // TODO: service get 로직으로 변경
+    this.event$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => of({ id: params.get("id")! })),
+    );
+  }
+}
--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from "@angular/common";
+import { HttpClientJsonpModule, HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { GoogleMapsModule } from "@angular/google-maps";
 
 import { EventDetailPageComponent } from "./event-detail-page/event-detail-page.component";
 import { EventRoutingModule } from "./event-routing.module";
 
 @NgModule({
   declarations: [EventDetailPageComponent],
-  imports: [CommonModule, EventRoutingModule],
+  imports: [
+    CommonModule,
+    EventRoutingModule,
+    GoogleMapsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+  ],
 })
 export class EventModule {}
