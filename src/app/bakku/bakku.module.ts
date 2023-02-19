73fe1c911383540ee27280202d499a426e5a6f94
Hyesung Oh <haesungoh414@gmail.com>
2023-02-19 21:38:45 +0900

    feat: markup bakku append page

--- src/app/bakku/bakku.module.ts
+++ src/app/bakku/bakku.module.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { GoogleMapsModule } from "@angular/google-maps";
+import { MatNativeDateModule } from "@angular/material/core";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
 
 import { BakkuAppendPageComponent } from "./bakku-append-page/bakku-append-page.component";
 import { BakkuDetailPageComponent } from "./bakku-detail-page/bakku-detail-page.component";
@@ -8,6 +13,15 @@ import { BakkuRoutingModule } from "./bakku-routing.module";
 
 @NgModule({
   declarations: [BakkuAppendPageComponent, BakkuDetailPageComponent],
-  imports: [CommonModule, BakkuRoutingModule, GoogleMapsModule],
+  imports: [
+    CommonModule,
+    BakkuRoutingModule,
+    GoogleMapsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
 export class BakkuModule {}
