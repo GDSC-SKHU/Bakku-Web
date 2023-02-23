34dd266116cad7fde87be4efc54d2a85168cd65c
Hyesung Oh <haesungoh414@gmail.com>
2023-02-23 23:42:25 +0900

    feat: apply bakku card component at ocean detail page

--- src/app/shared/components/bakku-card/bakku-card.component.ts
+++ src/app/shared/components/bakku-card/bakku-card.component.ts
@@ -1,9 +1,14 @@
 import { Component, Input } from "@angular/core";
+import { Router } from "@angular/router";
 
 // TODO: Service에서 정의하기
 interface Bakku {
   id: number;
+  ocean: string;
   name: string;
+  titleImage: string;
+  date: string;
+  cleanWeight: number;
 }
 
 @Component({
@@ -13,4 +18,11 @@ interface Bakku {
 })
 export class BakkuCardComponent {
   @Input() bakku!: Bakku;
+
+  constructor(private router: Router) {}
+
+  onCardClick = () => {
+    // this.router.navigate(["bakku", this.bakku.id]);
+    this.router.navigate(["bakku", "1"]);
+  };
 }
--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
 
 import { BakkuCardComponent } from "./bakku-card/bakku-card.component";
 
 @NgModule({
   declarations: [BakkuCardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   exports: [BakkuCardComponent],
 })
 export class ComponentsModule {}
