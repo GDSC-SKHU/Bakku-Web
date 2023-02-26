06e5681bae0522615e886da35557c29783789fdd
Hyesung Oh <haesungoh414@gmail.com>
2023-02-26 21:26:06 +0900

    feat: mocking events at home, append routing logic

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { of } from "rxjs";
+
+const MOCK_EVENTS = [
+  { id: 1, name: "을왕리 어쩌구 축제" },
+  { id: 2, name: "을왕리 어쩌구 축제" },
+];
 
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  constructor(private router: Router) {}
+
+  events$ = of(MOCK_EVENTS);
+
+  onEventClick = (id: number) => {
+    this.router.navigate(["event", id]);
+  };
+}
