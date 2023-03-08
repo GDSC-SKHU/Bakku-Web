b3577b75b66e668c274a20eb57171011f8bf2aa3
Hyesung Oh <haesungoh414@gmail.com>
2023-03-08 20:24:48 +0900

    feat: set default user data and logout logic at mypage

--- src/app/mypage/mypage/mypage.component.ts
+++ src/app/mypage/mypage/mypage.component.ts
@@ -1,8 +1,17 @@
 import { Component } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-mypage",
   templateUrl: "./mypage.component.html",
   styleUrls: ["./mypage.component.scss"],
 })
-export class MypageComponent {}
+export class MypageComponent {
+  constructor(public auth: AngularFireAuth, private router: Router) {}
+
+  logout() {
+    this.auth.signOut();
+    this.router.navigate(["/login"]);
+  }
+}
