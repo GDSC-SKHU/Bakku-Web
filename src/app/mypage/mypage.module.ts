326538146012774301f9983f3d6fa836a6a99a8b
Hyesung Oh <haesungoh414@gmail.com>
2023-03-09 19:39:31 +0900

    feat: publishing mypage

--- src/app/mypage/mypage.module.ts
+++ src/app/mypage/mypage.module.ts
@@ -1,11 +1,19 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 
+import { ComponentsModule } from "../shared/components/components.module";
 import { MypageComponent } from "./mypage/mypage.component";
 import { MypageRoutingModule } from "./mypage-routing.module";
 
 @NgModule({
   declarations: [MypageComponent],
-  imports: [CommonModule, MypageRoutingModule, MypageRoutingModule],
+  imports: [
+    CommonModule,
+    MypageRoutingModule,
+    MatButtonModule,
+    ComponentsModule,
+    MypageRoutingModule,
+  ],
 })
 export class MypageModule {}
