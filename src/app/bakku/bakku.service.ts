f82509b193a94931b5793b943368120802b4ebba
Hyesung Oh <haesungoh414@gmail.com>
2023-03-09 20:33:07 +0900

    feat: append get bakkus by uid, handle empty at mypage bakku

--- src/app/bakku/bakku.service.ts
+++ src/app/bakku/bakku.service.ts
@@ -1,8 +1,26 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+
+export interface Bakku {
+  id: number;
+  comment: string;
+  cleanWeight: number;
+  decorateTime: string;
+  titleImageUrl: string;
+  beforeImageUrl: string;
+  afterImageUrl: string;
+  groupName: string;
+  oceanId: number;
+  oceanName: string;
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class BakkuService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  getBakkusByUid(uid: string) {
+    return this.http.get<Paging<Bakku>>(`/bakkus?uid=${uid}`);
+  }
 }
--- src/app/mypage/mypage/mypage.component.ts
+++ src/app/mypage/mypage/mypage.component.ts
@@ -1,17 +1,43 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { Router } from "@angular/router";
+import { Bakku, BakkuService } from "src/app/bakku/bakku.service";
 
 @Component({
   selector: "app-mypage",
   templateUrl: "./mypage.component.html",
   styleUrls: ["./mypage.component.scss"],
 })
-export class MypageComponent {
-  constructor(public auth: AngularFireAuth, private router: Router) {}
+export class MypageComponent implements OnInit {
+  constructor(
+    public auth: AngularFireAuth,
+    private router: Router,
+    private bakkuService: BakkuService,
+  ) {}
+
+  bakkus: Bakku[] = [];
+  isEmpty: boolean = false;
 
   logout() {
     this.auth.signOut();
     this.router.navigate(["/login"]);
+  }
+
+  ngOnInit() {
+    this.getBakkusByUid();
+  }
+
+  getBakkusByUid() {
+    this.auth.user.subscribe((user) => {
+      this.bakkuService.getBakkusByUid(user!.uid).subscribe((res) => {
+        if (!res) {
+          this.isEmpty = true;
+          return;
+        }
+
+        this.isEmpty = res.empty;
+        this.bakkus = res.content;
+      });
+    });
   }
 }
