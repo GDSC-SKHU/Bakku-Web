a3892ca1eb57dd76ee4aab1258f668dd8a730267
Hyesung Oh <haesungoh414@gmail.com>
2023-03-10 20:40:03 +0900

    style: re-layout when mobile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import {
 } from "@angular/fire/compat/performance";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-import { MatSidenavModule } from "@angular/material/sidenav";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
@@ -44,7 +43,6 @@ import { httpInterceptorProviders } from "./shared/http-interceptors";
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatSidenavModule,
     LayoutModule,
     RankingModule,
     EventModule,
