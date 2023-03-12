ed0cf477c4b15d683b8309a0ef8ac7d4ca8833ba
Hyesung Oh <haesungoh414@gmail.com>
2023-03-12 18:27:16 +0900

    feat: set bakku card interface within service

--- src/app/shared/components/bakku-card/bakku-card.component.ts
+++ src/app/shared/components/bakku-card/bakku-card.component.ts
@@ -1,15 +1,6 @@
 import { Component, Input } from "@angular/core";
 import { Router } from "@angular/router";
-
-// TODO: Bakku service에서 정의된 인터페이스 활용하기
-interface Bakku {
-  id: number;
-  ocean: string;
-  name: string;
-  titleImage: string;
-  date: string;
-  cleanWeight: number;
-}
+import { Bakku } from "src/app/bakku/bakku.service";
 
 @Component({
   selector: "app-bakku-card",
@@ -22,7 +13,6 @@ export class BakkuCardComponent {
   constructor(private router: Router) {}
 
   onCardClick = () => {
-    // this.router.navigate(["bakku", this.bakku.id]);
-    this.router.navigate(["bakku", "1"]);
+    this.router.navigate(["bakku", this.bakku.id]);
   };
 }
