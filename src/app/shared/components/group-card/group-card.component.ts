156610f008e3d38997d8482e46b387f4503aca92
Hyesung Oh <haesungoh414@gmail.com>
2023-03-13 20:47:35 +0900

    feat: group card at total rank

--- src/app/shared/components/group-card/group-card.component.ts
+++ src/app/shared/components/group-card/group-card.component.ts
@@ -1,8 +1,31 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
+import { Group, SortTermType } from "src/app/ranking/ranking.service";
 
 @Component({
   selector: "app-group-card",
   templateUrl: "./group-card.component.html",
   styleUrls: ["./group-card.component.scss"],
 })
-export class GroupCardComponent {}
+export class GroupCardComponent implements OnInit {
+  @Input() group!: Group;
+  @Input() sortTerm!: SortTermType;
+
+  content = "";
+
+  ngOnInit() {
+    this.content = this.getContentBySortTerm();
+  }
+
+  getContentBySortTerm(): string {
+    switch (this.sortTerm) {
+      case "count":
+        return `총 ${this.group.totalCount}회 바꾸했어요.`;
+
+      case "weight":
+        return `총 ${this.group.totalWeight}kg 바꾸했어요.`;
+
+      default:
+        throw new Error("not expected error");
+    }
+  }
+}
